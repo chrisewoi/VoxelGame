92476df44f15b34e71e3702a0ab12d2f688b2eac
chrisewoi <69186935+chrisewoi@users.noreply.github.com>
2025-06-04 14:27:26 +1000

    Bot light basics working

--- Assets/BotLook.cs
+++ Assets/BotLook.cs
@@ -4,17 +4,47 @@ public class BotLook : MonoBehaviour
 {
     public Vector3 destinationPoint;
     public float destinationRadius;
-    public 
+    public float lightIOSpeed;
+    public float updateRateMin;
+    public float updateRateMax;
+    private float timer;
+
+    public DebugShowVelocity destination;
+
+    private Light light;
+    private float lightIntensityDefault;
+    private float lightIntensityMult;
+    
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        light = GetComponent<Light>();
+        lightIntensityDefault = light.intensity;
     }
 
     // Update is called once per frame
     void Update()
     {
+        destinationPoint = destination.GetLookPoint();
         
+        transform.LookAt(destinationPoint);
+
+        if (destination.GetVelocity().magnitude < 10f)
+        {
+            lightIntensityMult -= Time.deltaTime * lightIOSpeed;
+        }
+        else
+        {
+            lightIntensityMult += Time.deltaTime;
+        }
+
+        lightIntensityMult = Mathf.Clamp01(lightIntensityMult);
+
+        //if(timer > )
+        light.intensity = lightIntensityDefault * lightIntensityMult;
+        //Debug.Log("Vel Mag: " + destination.GetVelocity().magnitude);
+
+        timer += Time.deltaTime;
     }
 }
--- Assets/DebugShowVelocity.cs
+++ Assets/DebugShowVelocity.cs
@@ -16,4 +16,14 @@ public class DebugShowVelocity : MonoBehaviour
         Gizmos.DrawLine(transform.position, transform.position + kinematicCharacterMotor.BaseVelocity);
         //Debug.Log(kinematicCharacterMotor.BaseVelocity);
     }
+
+    public Vector3 GetLookPoint()
+    {
+        return transform.position + kinematicCharacterMotor.BaseVelocity;
+    }
+
+    public Vector3 GetVelocity()
+    {
+        return kinematicCharacterMotor.BaseVelocity;
+    }
 }
