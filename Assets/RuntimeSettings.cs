2f8142e29567c95fde962afddc6a618ef34ff768
chrisewoi <69186935+chrisewoi@users.noreply.github.com>
2025-06-19 15:45:43 +1000

    Terrain Forming

--- Assets/RuntimeSettings.cs
+++ Assets/RuntimeSettings.cs
@@ -1,5 +1,6 @@
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RuntimeSettings : MonoBehaviour
 {
@@ -9,17 +10,28 @@ public class RuntimeSettings : MonoBehaviour
     public string Notes = "Put lighting in this hierarchy for use in the editor. It will be disabled on runtime.\nhttps://www.freepik.com/free-photo/abstract-background-rock-texture_14622174.htm Image by freepik";
     
     private Light[] _lights;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public int frameRate;
+
     void Start()
     {
         RenderSettings.fog = true;
         _lights = GetComponentsInChildren<Light>();
         foreach(Light light in _lights) Destroy(light.gameObject);
+        Application.targetFrameRate = frameRate;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("r"))
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Application.Quit();
+        }
     }
 }
