0f4b51e7603def7abd8eafc5b706a619b89b4e7f
chrisewoi <69186935+chrisewoi@users.noreply.github.com>
2025-06-22 14:34:35 +1000

    Tunnel lock works

---
+++ Assets/TunnelRocks.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class TunnelRocks : MonoBehaviour
+{
+    public GameObject[] fires;
+    public bool autoUnlocked;
+    private bool unlocked = false;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        unlocked = autoUnlocked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!unlocked)
+        {
+            bool allFiresOn = true; //set trigger to true
+
+            foreach (GameObject fire in fires) // for each fire, 
+            {
+                if (!fire.activeInHierarchy) allFiresOn = false; //if any fires are off, set trigger to false
+            }
+
+            if (allFiresOn) unlocked = true; // if trigger is still on, all fires must be on, so unlock the tunnel!
+        }
+        else
+        {
+            gameObject.SetActive(false); // if unlocked, disable this gameObject (the tunnel rocks).
+        }
+    }
+}
